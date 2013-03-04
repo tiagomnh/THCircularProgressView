fd9f0dd1a70a5ac86a38fbf40dd4cd219f370d6a
Tiago Henriques <tiagomnh@gmail.com>
2013-03-04 22:47:39 +0000

    Adds examples of view usage

--- Demo/THCircularProgressView-Demo/THViewController.h
+++ Demo/THCircularProgressView-Demo/THViewController.h
@@ -8,6 +8,8 @@
 
 #import <UIKit/UIKit.h>
 
+#import "THCircularProgressView.h"
+
 @interface THViewController : UIViewController
 
 @end
--- Demo/THCircularProgressView-Demo/THViewController.m
+++ Demo/THCircularProgressView-Demo/THViewController.m
@@ -18,12 +18,62 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    
+    self.view.backgroundColor = [UIColor whiteColor];
+    
+    NSLog(@"%f %f", self.view.frame.size.width, self.view.frame.size.height);
+        
+    CGFloat width = self.view.frame.size.width;
+    CGFloat height = self.view.frame.size.height;
+    CGFloat radius = width / 4 * 0.8;
+    
+    THCircularProgressView *example1 = [[THCircularProgressView alloc] initWithCenter:CGPointMake(width * .25, height * .25)
+                                                                               radius:radius
+                                                                            lineWidth:20.0f
+                                                                        progressColor:[UIColor redColor]
+                                                                             fillType:THCircularProgressBarFillTypeCircle
+                                                                            fillColor:[UIColor colorWithRed:0.96f green:0.96f blue:0.96f alpha:1.00f]
+                                                                           percentage:0.3f];
+    [self.view addSubview:example1];
+    
+    
+    THCircularProgressView *example2 = [[THCircularProgressView alloc] initWithCenter:CGPointMake(width * .75, height * .25)
+                                                                               radius:radius
+                                                                            lineWidth:20.0f
+                                                                        progressColor:[UIColor greenColor]
+                                                                             fillType:THCircularProgressBarFillTypeCircumference
+                                                                            fillColor:[UIColor colorWithRed:0.96f green:0.96f blue:0.96f alpha:1.00f]
+                                                                           percentage:0.3f];
+    [self.view addSubview:example2];
+    
+    THCircularProgressView *example3 = [[THCircularProgressView alloc] initWithCenter:CGPointMake(width * .25, height * .75)
+                                                                               radius:radius
+                                                                            lineWidth:radius
+                                                                        progressColor:[UIColor blueColor]
+                                                                             fillType:THCircularProgressBarFillTypeCircle
+                                                                            fillColor:[UIColor colorWithRed:0.96f green:0.96f blue:0.96f alpha:1.00f]
+                                                                           percentage:0.3f];
+    [self.view addSubview:example3];
+
+    THCircularProgressView *example4 = [[THCircularProgressView alloc] initWithCenter:CGPointMake(width * .75, height * .75)
+                                                                               radius:radius
+                                                                            lineWidth:radius
+                                                                        progressColor:[UIColor purpleColor]
+                                                                             fillType:THCircularProgressBarFillTypeCircle
+                                                                            fillColor:[UIColor clearColor]
+                                                                           percentage:0.3f];
+    [self.view addSubview:example4];
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (BOOL)shouldAutorotate
+{
+    return false;
 }
 
 @end
